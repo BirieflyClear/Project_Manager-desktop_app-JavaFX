913343319872e99badda9017ec514e5196e33485
BrieflyClear <andrzej.przybysz01@gmail.com>
2020-02-12 21:45:04 +0100

    added main layouts and left layouts with ScrollPane

--- src/main/java/com/lazydev/project_manager/utilities/Constants.java
+++ src/main/java/com/lazydev/project_manager/utilities/Constants.java
@@ -2,7 +2,7 @@ package com.lazydev.project_manager.utilities;
 
 public class Constants {
 
-    public static String FXML_HOME = "/fxml/Home.fxml";
+    public static String FXML_HOME = "/fxml/MainWindow.fxml";
     public static String FXML_ITEM_TASK = "/fxml/TaskItem.fxml";
     public static String ICON_CHECK_UNFILL = "/icons/icons8_checked_filled_24px.png";
     public static String ICON_CHECK_FILL = "/icons/icons8_checked_filled_24px_1.png";
