6c4423328073b42ed90970d42029796cf365cefb
BrieflyClear <andrzej.przybysz01@gmail.com>
2020-02-12 14:42:27 +0100

    added Main class and Constants for app's consts

---
+++ src/java/com/lazydev/project_manager/Main.java
@@ -0,0 +1,48 @@
+package com.lazydev.project_manager;
+
+import com.lazydev.project_manager.utilities.Constants;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import java.net.URL;
+
+public class Main extends Application {
+
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        URL path = getClass().getResource(Constants.FXML_HOME);
+        if (path != null) {
+            Parent root = FXMLLoader.load(path);
+            Scene scene = new Scene(root);
+
+            stage.setScene(scene);
+            stage.setTitle(Constants.APP_TITLE);
+            stage.initStyle(StageStyle.TRANSPARENT);
+            // Grab your root here
+            root.setOnMousePressed((MouseEvent event) -> {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            });
+            // Move around here
+            root.setOnMouseDragged((MouseEvent event) -> {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            });
+            stage.show();
+        } else {
+            System.exit(-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/java/com/lazydev/project_manager/utilities/Constants.java
@@ -0,0 +1,10 @@
+package com.lazydev.project_manager.utilities;
+
+public class Constants {
+
+    public static String FXML_HOME = "/fxml/Home.fxml";
+    public static String FXML_ITEM_TASK = "/fxml/TaskItem.fxml";
+    public static String ICON_CHECK_UNFILL = "/icons/icons8_checked_filled_24px.png";
+    public static String ICON_CHECK_FILL = "/icons/icons8_checked_filled_24px_1.png";
+    public static String APP_TITLE = "Project Manager - all your ideas in one place";
+}
